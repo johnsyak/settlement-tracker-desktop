d79592ca18a5c0ac77341eb3e54a61e3edd3d94c
johnsyak <johnyak381@gmail.com>
2018-12-15 15:42:13 -0500

    first commit

---
+++ src/main/java/com/settlement_tracker_desktop/settlement_tracker/SettlementTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.settlement_tracker_desktop.settlement_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementTrackerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/settlement_tracker_desktop/settlement_tracker/SettlementTrackerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.settlement_tracker_desktop.settlement_tracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SettlementTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
